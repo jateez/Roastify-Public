fd5223a32919dac777f92f44e3e528cbfe01d7de
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-08-07 16:51:05 +0700

    feat: add routes for get all roast, delete a roast, and get a roast. add endpoints for profile, and spotifys

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const Controller = require("../controllers/index")
 const SpotifyController = require("../controllers/SpotifyController")
+const mutlerUpload = require("../middlewares/mutler")
 
 const router = require("express").Router()
 
@@ -7,8 +8,12 @@ router.get("/", Controller.index)
 router.post("/register", Controller.register)
 router.post("/login", Controller.loginEmail)
 router.post("/google-login", Controller.loginGoogle)
+router.get("/roasts", Controller.getRoast)
+router.get("/roasts/:roastId", Controller.getRoastByID)
+router.delete("/roasts/:roastId", Controller.deleteRoastById)
 router.get("/spotify-login", SpotifyController.login)
 router.get("/spotify-callback", SpotifyController.callback)
 router.get("/spotify/roast", SpotifyController.getUserRoast)
-
+router.put("/profile", mutlerUpload, Controller.updateProfile)
+router.patch("/profile", mutlerUpload, Controller.uploadImage)
 module.exports = { router }
